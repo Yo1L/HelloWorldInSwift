8922ab1f45d6f54ce92599cd93d551ce38322778
Yoann Le Viavant <yoann.leviavant@kompetentia.fr>
2017-12-26 23:07:13 +0100

    Initial Commit

---
+++ HelloWorldInSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldInSwift
+//
+//  Created by Yoann Le Viavant on 26/12/2017.
+//  Copyright © 2017 Yoann Le Viavant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
